62dee6fad4a8997070a6a81bc4694fb6b032944f
ecommand <devnull@localhost>
2008-10-28 10:32:52 +0000

    removed some legacy code

--- src/main/java/com/eg/gwt/openLayers/public/js/gwt-openlayers/relay.js
+++ src/main/java/com/eg/gwt/openLayers/public/js/gwt-openlayers/relay.js
@@ -23,7 +23,7 @@ if(!com){
     }
 }
 
-//convert Objects to Array
+//function for converting Object to Array
 com.eg.gwt.openLayers.client.convertToArray = function(o){
     var a = new Array();
     for(var i = 0, m = o.length; i < m; i++){
@@ -32,7 +32,9 @@ com.eg.gwt.openLayers.client.convertToArray = function(o){
     return a;
 }
 
+//relay functions
 com.eg.gwt.openLayers.client.relay = {
+
         /* copy, paste directly below here, adjust name of function, and specify body
         exampleRelayFnt : function(o){
         },
@@ -59,50 +61,3 @@ com.eg.gwt.openLayers.client.relay = {
             vector.addFeatures(com.eg.gwt.openLayers.client.convertToArray(o));
         }
 }
-
-
-//LEGACY
-/*
-var gwt = {
-    openlayers : {
-        relay : {
-       
-//          copy, paste directly below here, adjust name of function, and specify body
-//            exampleRelayFnt : function(o){
-//            },
-            
-    
-            //paste new relay function here
-
-            createPolygon : function(o){
-                return new OpenLayers.Geometry.Polygon(
-                        gwt.openlayers.convertToArray(o));
-            },
-            
-            createLinearRing : function(o){
-                return new OpenLayers.Geometry.LinearRing(
-                        gwt.openlayers.convertToArray(o));
-            },
-            
-            createLineString : function(o){
-                return new OpenLayers.Geometry.LineString(
-                        gwt.openlayers.convertToArray(o));
-            },
-            
-            vectorAddFeatures : function(vector, o){
-                vector.addFeatures(gwt.openlayers.convertToArray(o));
-            }
-        },
-        
-        //convert array-like Object to array of type Array
-        //object should be an array created using JObjectArray
-        convertToArray : function(o){
-            var a = new Array();
-            for(var i = 0, m = o.length; i < m; i++){
-                a[i] = o[i];
-            }
-            return a;
-        }
-    }
-}
-*/

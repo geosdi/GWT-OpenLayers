e10093357e1a78ead7fa8d2f69ffb3bd34c8701c
ecommand <devnull@localhost>
2008-11-04 12:19:07 +0000

    added ScaleLine Control

---
+++ src/main/java/com/eg/gwt/openLayers/client/control/ScaleLine.java
@@ -0,0 +1,20 @@
+package com.eg.gwt.openLayers.client.control;
+
+import com.eg.gwt.openLayers.client.JSObject;
+
+/**
+ * 
+ * @author Edwin Commandeur - Atlis EJS
+ *
+ */
+public class ScaleLine extends Control {
+
+    protected ScaleLine(JSObject element) {
+        super(element);
+    }
+    
+    public ScaleLine(){
+        this(ScaleLineImpl.create());
+    }
+
+}

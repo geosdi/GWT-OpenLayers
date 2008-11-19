ac292741f51841185bfddbddb72ee822a1887dbf
ecommand <devnull@localhost>
2008-11-19 14:32:36 +0000

    *** empty log message ***

---
+++ src/main/java/com/eg/gwt/openLayers/public/js/gwt-openlayers/util.js
@@ -0,0 +1,79 @@
+/*
+ * Utility class with helper functions and workarounds for problematic JSNI cases. 
+ * 
+ * gwt_openlayers_util.relay
+ *   relays calls to OpenLayers functions that taken an Array parameter
+ *   and test is instanceof Array.
+ *
+ * @author 
+ */
+
+//make namespacing object
+if(!gwt_openlayers_util){ var gwt_openlayers_util = new Object(); }
+
+
+//function for converting Object to Array
+gwt_openlayers_util.convertToArray = function(o){
+    var a = new Array();
+    for(var i = 0, m = o.length; i < m; i++){
+        a[i] = o[i];
+    }
+    return a;
+}
+
+//function for converting JSNI created javascript Object to plain javascript Object
+//GWT created javascript objects fail instanceof Object test
+//  TO DO make recursive
+gwt_openlayers_util.convertToPlainObject = function(o){
+    var obj = new Object();
+    for(prop in o){
+        var x = prop;
+    }
+    return obj;
+}
+
+//to get around the test: if(this.eventListeners instanceof Object){ ...}
+gwt_openlayers_util.eventListenersToObject = function(options){
+    if(options.eventListeners){
+        var obj = new Object();
+        for(i in options.eventListeners){
+            alert(i)
+            obj[i] = options.eventListeners[i]
+        }
+        options.eventListeners = new Object();
+        for(j in obj){
+            options.eventListeners[j] = obj[j]
+        }
+    }
+    return options;
+} 
+
+//relay functions
+gwt_openlayers_util.relay = {
+
+        /* copy, paste directly below here, adjust name of function, and specify body
+        exampleRelayFnt : function(o){
+        },
+        */
+
+        //paste new relay function here
+
+        createPolygon : function(o){
+            return new OpenLayers.Geometry.Polygon(
+                    gwt_openlayers_util.convertToArray(o));
+        },
+        
+        createLinearRing : function(o){
+            return new OpenLayers.Geometry.LinearRing(
+                    gwt_openlayers_util.convertToArray(o));
+        },
+        
+        createLineString : function(o){
+            return new OpenLayers.Geometry.LineString(
+                    gwt_openlayers_util.convertToArray(o));
+        },
+        
+        vectorAddFeatures : function(vector, o){
+            vector.addFeatures(gwt_openlayers_util.convertToArray(o));
+        }
+}

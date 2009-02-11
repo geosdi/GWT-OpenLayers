0dd6a097c9dce2ecd6de20b5a2d3713bd0fe6bd5
ecommand <devnull@localhost>
2009-02-11 09:26:19 +0000

    fixed drawfeature (JSNI reference to JSObject, which is now in util)

--- src/main/java/com/eg/gwt/openLayers/client/control/DrawFeatureImpl.java
+++ src/main/java/com/eg/gwt/openLayers/client/control/DrawFeatureImpl.java
@@ -4,7 +4,7 @@ import com.eg.gwt.openLayers.client.event.FeatureAddedListener;
 import com.eg.gwt.openLayers.client.util.JSObject;
 
 /**
- * 
+ *
  * @author Erdem Gunay
  * @author Edwin Commandeur - Atlis EJS
  *
@@ -16,24 +16,24 @@ class DrawFeatureImpl {
     }-*/;
 
     //TODO reduce these three methods to one with DrawFeatureOptions
-    
+
     public static native JSObject create(JSObject layer, JSObject handler, JSObject options)/*-{
         return new $wnd.OpenLayers.Control.DrawFeature(layer, handler, options);
     }-*/;
 
     public static native JSObject create(JSObject layer, JSObject handler, FeatureAddedListener listener)/*-{
         var callback = function(obj){
-            listener.@com.eg.gwt.openLayers.client.event.FeatureAddedListener::onFeatureAdded(Lcom/eg/gwt/openLayers/client/JSObject;)(obj);
+            listener.@com.eg.gwt.openLayers.client.event.FeatureAddedListener::onFeatureAdded(Lcom/eg/gwt/openLayers/client/util/JSObject;)(obj);
         }
         return new $wnd.OpenLayers.Control.DrawFeature(layer, handler, {featureAdded:callback});
     }-*/;
-    
+
     public static native JSObject create(JSObject layer, JSObject handler, FeatureAddedListener listener, JSObject options)/*-{
         var callback = function(obj){
-            listener.@com.eg.gwt.openLayers.client.event.FeatureAddedListener::onFeatureAdded(Lcom/eg/gwt/openLayers/client/JSObject;)(obj);
+            listener.@com.eg.gwt.openLayers.client.event.FeatureAddedListener::onFeatureAdded(Lcom/eg/gwt/openLayers/client/util/JSObject;)(obj);
         }
         options.featureAdded = callback;
         return new $wnd.OpenLayers.Control.DrawFeature(layer, handler, options);
-    }-*/;    
+    }-*/;
 
 }

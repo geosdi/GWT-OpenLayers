7fdaab2c9ff55e558438587795c67719b91293f5
ecommand <devnull@localhost>
2009-02-27 11:43:17 +0000

    Changed projection to extend JSObjectWrapper, as Projection has no events property, added a getter, and some JavaDoc.

--- src/main/java/com/eg/gwt/openLayers/client/Projection.java
+++ src/main/java/com/eg/gwt/openLayers/client/Projection.java
@@ -1,24 +1,39 @@
 package com.eg.gwt.openLayers.client;
 
 import com.eg.gwt.openLayers.client.util.JSObject;
+import com.eg.gwt.openLayers.client.util.JSObjectWrapper;
 
-public class Projection extends OpenLayersWidget {
+/**
+ * From the OpenLayers API docs:
+ * Class for coordinate transforms between coordinate systems.
+ *
+ * @author Evgeny Gazdovsky
+ * @author Edwin Commandeur - Atlis EJS
+ *
+ */
+public class Projection extends JSObjectWrapper {
 
 	protected Projection(JSObject element) {
 		super(element);
 	}
 
-	public Projection(String projection){
-		this (ProjectionImpl.create(projection));
+	/**
+	 *
+	 * @param projectionCode - projection code that identifies the projection or an SRS in a
+	 *   repository that contains projection and SRS definitions, such as the EPSG registry.
+	 *   An example of an EPSG SRS code is EPSG:4326, which is typically projected in equidistant
+	 *   cylindrical projection.
+	 */
+	public Projection(String projectionCode){
+		this (ProjectionImpl.create(projectionCode));
 	}
-
 
 	/**
 	 *
-	 * @param code -
+	 * @return String - projection code, see {@link #Projection(String)}.
 	 */
-	public void setProjectionCode(String code){
-		ProjectionImpl.setProjectionCode(getJSObject(), code);
+	public String getProjectionCode(String projectionCode){
+		return ProjectionImpl.getProjectionCode(getJSObject());
 	}
 
 }
--- src/main/java/com/eg/gwt/openLayers/client/ProjectionImpl.java
+++ src/main/java/com/eg/gwt/openLayers/client/ProjectionImpl.java
@@ -2,13 +2,19 @@ package com.eg.gwt.openLayers.client;
 
 import com.eg.gwt.openLayers.client.util.JSObject;
 
+/**
+ *
+ * @author Evgeny Gazdovsky
+ * @author Edwin Commandeur - Atlis EJS
+ *
+ */
 public class ProjectionImpl {
 
 	public static native JSObject create(String projection)/*-{
 		return new $wnd.OpenLayers.Projection(projection);
 	}-*/;
 
-	public static native void setProjectionCode(JSObject self, String code)/*-{
-		self.projCode = code;
+	public static native String getProjectionCode(JSObject self)/*-{
+		return self.getCode();
 	}-*/;
 }

100d5933857cb13341cc78c755fe19ed2f425713
jensenc <devnull@localhost>
2009-03-10 18:56:38 +0000

    Added getCenterLonLat and popup methods.

--- src/main/java/com/eg/gwt/openLayers/client/feature/Feature.java
+++ src/main/java/com/eg/gwt/openLayers/client/feature/Feature.java
@@ -1,11 +1,16 @@
 package com.eg.gwt.openLayers.client.feature;
 
+import com.eg.gwt.openLayers.client.Bounds;
+import com.eg.gwt.openLayers.client.LonLat;
 import com.eg.gwt.openLayers.client.OpenLayersWidget;
+import com.eg.gwt.openLayers.client.Style;
+import com.eg.gwt.openLayers.client.popup.Popup;
 import com.eg.gwt.openLayers.client.util.JSObject;
 
 /**
  * 
  * @author Edwin Commandeur - Atlis EJS
+ * @author Curtis Jensen
  * 
  */
 public abstract class Feature extends OpenLayersWidget {
@@ -21,4 +26,30 @@ public abstract class Feature extends OpenLayersWidget {
     public String getFeatureId(){
         return FeatureImpl.getFeatureId(getJSObject());
     }
+
+	public Popup createPopup(boolean closeBox) {
+		JSObject popupObj = FeatureImpl.createPopup(this.getJSObject(), closeBox);
+		
+		return Popup.narrowToOpenLayersPopup(popupObj);
+	}
+	
+	public LonLat getCenterLonLat() {
+		return Bounds.narrowToBounds(FeatureImpl.getBounds(this.getJSObject())).getCenterLonLat();
+	}
+	
+	public void setPopup(Popup popup) {
+		FeatureImpl.setPopup(this.getJSObject(), popup.getJSObject());
+	}
+	
+	public Popup getPopup() {
+		return Popup.narrowToOpenLayersPopup(FeatureImpl.getPopup(this.getJSObject()));
+	}
+	
+	public void resetPopup() {
+		FeatureImpl.resetPopup(this.getJSObject());
+	}
+	
+	public void setStyle(Style newStyle) {
+		FeatureImpl.setStyle(this.getJSObject(), newStyle.getJSObject());
+	}
 }
--- src/main/java/com/eg/gwt/openLayers/client/feature/FeatureImpl.java
+++ src/main/java/com/eg/gwt/openLayers/client/feature/FeatureImpl.java
@@ -5,6 +5,7 @@ import com.eg.gwt.openLayers.client.util.JSObject;
 /**
  * 
  * @author Edwin Commandeur - Atlis EJS
+ * @author Curtis Jensen
  * 
  */
 public class FeatureImpl {
@@ -16,4 +17,29 @@ public class FeatureImpl {
     public static native String getFeatureId(JSObject self)/*-{
         return self.id;
     }-*/;    
+
+	public static native void setPopup(JSObject feature, JSObject inputPopup)/*-{
+		feature.popup = inputPopup;
+	}-*/;
+	
+	public static native JSObject getPopup(JSObject feature)/*-{
+		return feature.popup;
+	}-*/;
+	
+	public static native void resetPopup(JSObject feature)/*-{
+		feature.popup.destroy();
+		feature.popup = null;
+	}-*/;
+
+	public static native JSObject createPopup(JSObject feature, boolean closeBox)/*-{
+		return feature.createPopup(closeBox);
+	}-*/;
+	
+	public static native JSObject getBounds(JSObject feature)/*-{
+		return feature.geometry.getBounds();
+	}-*/;
+	
+	public static native void setStyle(JSObject feature, JSObject newStyle)/*-{
+		feature.style = newStyle;
+	}-*/;
 }

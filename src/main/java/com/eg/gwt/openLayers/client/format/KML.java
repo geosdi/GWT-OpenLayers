57b8c7e35e95b52c820f668121711ca879d3a441
jensenc <devnull@localhost>
2009-03-10 18:57:25 +0000

    Added read and write methods.

--- src/main/java/com/eg/gwt/openLayers/client/format/KML.java
+++ src/main/java/com/eg/gwt/openLayers/client/format/KML.java
@@ -1,5 +1,8 @@
 package com.eg.gwt.openLayers.client.format;
 
+import com.eg.gwt.openLayers.client.feature.Feature;
+import com.eg.gwt.openLayers.client.feature.VectorFeature;
+import com.eg.gwt.openLayers.client.util.JObjectArray;
 import com.eg.gwt.openLayers.client.util.JSObject;
 
 /**
@@ -10,6 +13,8 @@ import com.eg.gwt.openLayers.client.util.JSObject;
  * See http://www.ogc.org/
  *
  * @author Edwin Commandeur - Atlis EJS
+ * @author Curtis Jensen
+ *
  */
 public class KML extends VectorFormat {
 
@@ -21,4 +26,17 @@ public class KML extends VectorFormat {
 		this(KMLImpl.create());
 	}
 
+	public VectorFeature[] read(String kmlString) {
+		JSObject kmlFeatures = KMLImpl.read(this.getJSObject(), kmlString);
+		JSObject[] kmlFeatureArray = JObjectArray.narrowToJObjectArray(kmlFeatures).toArray();
+		VectorFeature[] features = new VectorFeature[kmlFeatureArray.length];
+		for (int i = 0; i < kmlFeatureArray.length; i++)
+			features[i] = VectorFeature.narrowToVectorFeature(kmlFeatureArray[i]);
+
+		return features;  
+	}
+	
+	public String write(Feature features, boolean pretty) {
+		return KMLImpl.write(this.getJSObject(), features.getJSObject(), pretty);
+	}
 }
--- src/main/java/com/eg/gwt/openLayers/client/format/KMLImpl.java
+++ src/main/java/com/eg/gwt/openLayers/client/format/KMLImpl.java
@@ -4,6 +4,7 @@ import com.eg.gwt.openLayers.client.util.JSObject;
 
 /**
  * @author Edwin Commandeur - Atlis EJS
+ * @author Curtis Jensen
  *
  */
 public class KMLImpl {
@@ -11,6 +12,14 @@ public class KMLImpl {
 	public static native JSObject create()
 	/*-{
 		return new $wnd.OpenLayers.Format.KML();
+	}-*/;
+
+	public static native JSObject read(JSObject kmlFormat, String kmlString)/*-{
+		return kmlFormat.read(kmlString);
+	}-*/;
+	
+	public static native String write(JSObject kmlFormat, JSObject feature, boolean pretty)/*-{
+		return kmlFormat.write(feature, pretty);
 	}-*/;
 
 }

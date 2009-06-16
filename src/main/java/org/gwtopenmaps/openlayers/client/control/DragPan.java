57cd0c73fa9d0b50e4ad724330318aae84d75487
Edwin Commandeur <commandeur.edwin@gmail.com>
2009-06-17 01:34:16 +0200

    DragPan has no API properties (therefore no Options).

--- src/main/java/org/gwtopenmaps/openlayers/client/control/DragPan.java
+++ src/main/java/org/gwtopenmaps/openlayers/client/control/DragPan.java
@@ -3,7 +3,10 @@ package org.gwtopenmaps.openlayers.client.control;
 import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 /**
+ * DragPan control wrapper.
+ *
  * @author Aaron Novstrup - Stottler Henke Associates, Inc.
+ *
  */
 public class DragPan extends Control {
 	protected DragPan(JSObject element) {
@@ -12,10 +15,6 @@ public class DragPan extends Control {
 
 	public DragPan() {
 		this(DragPanImpl.create());
-	}
-
-	public DragPan(DragPanOptions options) {
-		this(DragPanImpl.create(options.getJSObject()));
 	}
 
 }

ae299c60e6125cf8272a5dfe6fdbe5fb3ba4ac64
michelvitor2 <devnull@localhost>
2009-08-20 10:30:28 -0300

    create the correspondent

--- src/main/java/org/gwtopenmaps/openlayers/client/MarkerImpl.java
+++ src/main/java/org/gwtopenmaps/openlayers/client/MarkerImpl.java
@@ -10,14 +10,16 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
  */
 class MarkerImpl {
 
+	
 	public static native JSObject create(JSObject lonlat)/*-{
 		return new $wnd.OpenLayers.Marker(lonlat);
 	}-*/;
 	public static native void setIcon(JSObject self,JSObject icon)/*-{
-	     self.setIcon(icon);
+	     self.icon = icon;
+	     self.events= new $wnd.OpenLayers.Events(self,self.icon.imageDiv,null);     
     }-*/;
-	public static native JSObject setLonLat(JSObject lonlat)/*-{
-        self.setLonlat(lonlat);
+	public static native JSObject setLonLat(JSObject self,JSObject lonlat)/*-{
+        self.lonlat = lonlat;
     }-*/;
 	public static native JSObject create(JSObject lonlat, JSObject icon)/*-{
 		return new $wnd.OpenLayers.Marker(lonlat, icon);

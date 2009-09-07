2de3614922a772fb23c70578e2d29bed2749f0d5
ceravolo <devnull@localhost>
2009-09-07 16:46:12 -0300

    Ticket #4 - Add support for BackgroundGraphic and ZIndex in Style

--- src/main/java/org/gwtopenmaps/openlayers/client/Style.java
+++ src/main/java/org/gwtopenmaps/openlayers/client/Style.java
@@ -5,91 +5,140 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
 /**
  * @author Edwin Commandeur - Atlis EJS
  * @author Curtis Jensen
- *
+ * 
  */
-public class Style extends OpenLayersObjectWrapper{
-    
-    public static final String STROKE_DASHSTYLE_SOLID = "solid";
-    public static final String STROKE_DASHSTYLE_DASHDOT = "dashdot";
-    
-    
-    protected Style(JSObject element){
-        super(element);
-    }
-    
-    public Style() {
-        this(StyleImpl.create());
-    }
+public class Style extends OpenLayersObjectWrapper {
 
-    /**
-     * @param c - hexidecimal color code or a W3C standard color name 
-     */
-    public void setFillColor(String c){
-        StyleImpl.setFillColor(getJSObject(), c);
-    }
-    
-    public String getFillColor(){
-        return StyleImpl.getFillColor(getJSObject());
-    }
-    
-    public void setFillOpacity(double o){
-        StyleImpl.setFillOpacity(getJSObject(), o);
-    }
-    
-    public double getFillOpacity(){
-        double o = StyleImpl.getFillOpacity(getJSObject());
-        return o;
-    }
+	public static final String STROKE_DASHSTYLE_SOLID = "solid";
+	public static final String STROKE_DASHSTYLE_DASHDOT = "dashdot";
 
-    public void setPointRadius(double r){
-        StyleImpl.setPointRadius(getJSObject(), r);
-    }
+	protected Style(JSObject element) {
+		super(element);
+	}
 
-    public double getPointRadius(){
-        return StyleImpl.getPointRadius(getJSObject());
-    }
+	public Style() {
+		this(StyleImpl.create());
+	}
 
-    
-    /**
-     * @param c - see setFillColor 
-     */
-    public void setStrokeColor(String c){
-        StyleImpl.setStrokeColor(getJSObject(), c);
-    }
-    
-    public String getStrokeColor(){
-        return StyleImpl.getStrokeColor(getJSObject());
-    }
+	/**
+	 * @param c
+	 *            - hexidecimal color code or a W3C standard color name
+	 */
+	public void setFillColor(String c) {
+		StyleImpl.setFillColor(getJSObject(), c);
+	}
 
-    public void setStrokeWidth(double w){
-        StyleImpl.setStrokeWidth(getJSObject(), w);
-    }
-    
-    public double getStrokeWidth(){
-        return StyleImpl.getStrokeWidth(getJSObject());
-    }    
-    
-    public void setExternalGraphic(String graphicURL) {
-    	StyleImpl.setExternalGraphic(getJSObject(), graphicURL);
-    }
-    
-    public String getExternalGraphic() {
-    	return StyleImpl.getExternalGraphic(getJSObject());
-    }
-    
-    public void setGraphicSize(int width, int height) {
-    	StyleImpl.setGraphicSize(getJSObject(), width, height);
-    }
-    
-    public int getGraphicWidth() {
-    	return StyleImpl.getGraphicWidth(getJSObject());
-    }
-    
-    public int getGraphicHeight() {
-    	return StyleImpl.getGraphicHeight(getJSObject());
-    }
-    
-    public void setGraphicOffset(int xOffset, int yOffset) {
-    	StyleImpl.setGraphicOffset(getJSObject(), xOffset, yOffset);
-    }
+	public String getFillColor() {
+		return StyleImpl.getFillColor(getJSObject());
+	}
+
+	public void setFillOpacity(double o) {
+		StyleImpl.setFillOpacity(getJSObject(), o);
+	}
+
+	public double getFillOpacity() {
+		double o = StyleImpl.getFillOpacity(getJSObject());
+		return o;
+	}
+
+	public void setPointRadius(double r) {
+		StyleImpl.setPointRadius(getJSObject(), r);
+	}
+
+	public double getPointRadius() {
+		return StyleImpl.getPointRadius(getJSObject());
+	}
+
+	/**
+	 * @param c
+	 *            - see setFillColor
+	 */
+	public void setStrokeColor(String c) {
+		StyleImpl.setStrokeColor(getJSObject(), c);
+	}
+
+	public String getStrokeColor() {
+		return StyleImpl.getStrokeColor(getJSObject());
+	}
+
+	public void setStrokeWidth(double w) {
+		StyleImpl.setStrokeWidth(getJSObject(), w);
+	}
+
+	public double getStrokeWidth() {
+		return StyleImpl.getStrokeWidth(getJSObject());
+	}
+
+	public void setExternalGraphic(String graphicURL) {
+		StyleImpl.setExternalGraphic(getJSObject(), graphicURL);
+	}
+
+	public String getExternalGraphic() {
+		return StyleImpl.getExternalGraphic(getJSObject());
+	}
+
+	public void setGraphicSize(int width, int height) {
+		StyleImpl.setGraphicSize(getJSObject(), width, height);
+	}
+
+	public int getGraphicWidth() {
+		return StyleImpl.getGraphicWidth(getJSObject());
+	}
+
+	public int getGraphicHeight() {
+		return StyleImpl.getGraphicHeight(getJSObject());
+	}
+
+	public void setGraphicOffset(int xOffset, int yOffset) {
+		StyleImpl.setGraphicOffset(getJSObject(), xOffset, yOffset);
+	}
+
+	public void setBackgroundGraphicSize(int backgroundWidth, int backgroundHeight) {
+		StyleImpl.setBackgroundGraphicSize(getJSObject(), backgroundWidth, backgroundHeight);
+	}
+
+	public void setBackgroundHeight(int backgroundHeight) {
+		StyleImpl.setBackgroundHeight(getJSObject(), backgroundHeight);
+	}
+
+	public int getBackgroundHeight() {
+		return StyleImpl.getBackgroundHeight(getJSObject());
+	}
+
+	public void setBackgroundWidth(int backgroundWidth) {
+		StyleImpl.setBackgroundWidth(getJSObject(), backgroundWidth);
+	}
+
+	public int getBackgroundWidth() {
+		return StyleImpl.getBackgroundWidth(getJSObject());
+	}
+
+	public void setBackgroundGraphic(String graphicURL) {
+		StyleImpl.setBackgroundGraphic(getJSObject(), graphicURL);
+	}
+
+	public String getBackgroundGraphic() {
+		return StyleImpl.getBackgroundGraphic(getJSObject());
+	}
+
+	public void setBackgroundOffset(int backgroundXOffset, int backgroundYOffset) {
+		StyleImpl.setBackgroundOffset(getJSObject(), backgroundXOffset, backgroundYOffset);
+	}
+
+	public void setGraphicZIndex(int graphicZIndex) {
+		StyleImpl.setGraphicZIndex(getJSObject(), graphicZIndex);
+	}
+
+	public int getGraphicZIndex() {
+		return StyleImpl.getGraphicZIndex(getJSObject());
+	}
+
+	public void setBackgroundGraphicZIndex(int backgroundGraphicZIndex) {
+		StyleImpl.setBackgroundGraphicZIndex(getJSObject(), backgroundGraphicZIndex);
+	}
+
+	public int getBackgroundGraphicZIndex() {
+		return StyleImpl.getBackgroundGraphicZIndex(getJSObject());
+	}
+
 }
--- src/main/java/org/gwtopenmaps/openlayers/client/StyleImpl.java
+++ src/main/java/org/gwtopenmaps/openlayers/client/StyleImpl.java
@@ -12,92 +12,154 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
  */
 public class StyleImpl {
 
-    public static native JSObject create()
-    /*-{
-        return $wnd.OpenLayers.Util.extend({}, $wnd.OpenLayers.Feature.Vector.style['default']);
-        //return new Object(); //do it all yourself
-    }-*/;
-    
-    public static native void setFillColor(JSObject self, String s)
-    /*-{
-        self.fillColor = s;
-    }-*/;
-
-    public static native String getFillColor(JSObject self)
-    /*-{
-        return self.fillColor;
-    }-*/;
-    
-    public static native void setFillOpacity(JSObject self, double o)
-    /*-{
-        self.fillOpacity = o;
-    }-*/;
-
-    public static native double getFillOpacity(JSObject self)
-    /*-{
-        return self.fillOpacity;
-    }-*/;
-
-    public static native void setPointRadius(JSObject self, double o)
-    /*-{
-        self.pointRadius = o;
-    }-*/;
-
-    public static native double getPointRadius(JSObject self)
-    /*-{
-        return self.pointRadius;
-    }-*/;
-
-    public static native void setStrokeColor(JSObject self, String s)
-    /*-{
-        self.strokeColor = s;
-    }-*/;
-
-    public static native String getStrokeColor(JSObject self)
-    /*-{
-        return self.strokeColor;
-    }-*/;    
-
-    public static native void setStrokeWidth(JSObject self, double w)
-    /*-{
-        self.strokeWidth = w;
-    }-*/;
-
-    public static native double getStrokeWidth(JSObject self)
-    /*-{
-        return self.strokeWidth;
-    }-*/;
-    
-    public static native void setExternalGraphic(JSObject self, String graphicURL)
-    /*-{
-    	self.externalGraphic = graphicURL;
+	public static native JSObject create()
+	/*-{
+	    return $wnd.OpenLayers.Util.extend({}, $wnd.OpenLayers.Feature.Vector.style['default']);
+	    //return new Object(); //do it all yourself
 	}-*/;
-    
-    public static native String getExternalGraphic(JSObject self)
-    /*-{
+
+	public static native void setFillColor(JSObject self, String s)
+	/*-{
+	    self.fillColor = s;
+	}-*/;
+
+	public static native String getFillColor(JSObject self)
+	/*-{
+	    return self.fillColor;
+	}-*/;
+
+	public static native void setFillOpacity(JSObject self, double o)
+	/*-{
+	    self.fillOpacity = o;
+	}-*/;
+
+	public static native double getFillOpacity(JSObject self)
+	/*-{
+	    return self.fillOpacity;
+	}-*/;
+
+	public static native void setPointRadius(JSObject self, double o)
+	/*-{
+	    self.pointRadius = o;
+	}-*/;
+
+	public static native double getPointRadius(JSObject self)
+	/*-{
+	    return self.pointRadius;
+	}-*/;
+
+	public static native void setStrokeColor(JSObject self, String s)
+	/*-{
+	    self.strokeColor = s;
+	}-*/;
+
+	public static native String getStrokeColor(JSObject self)
+	/*-{
+	    return self.strokeColor;
+	}-*/;
+
+	public static native void setStrokeWidth(JSObject self, double w)
+	/*-{
+	    self.strokeWidth = w;
+	}-*/;
+
+	public static native double getStrokeWidth(JSObject self)
+	/*-{
+	    return self.strokeWidth;
+	}-*/;
+
+	public static native void setExternalGraphic(JSObject self, String graphicURL)
+	/*-{
+		self.externalGraphic = graphicURL;
+	}-*/;
+
+	public static native String getExternalGraphic(JSObject self)
+	/*-{
 		return self.externalGraphic;
 	}-*/;
 
-    public static native void setGraphicSize(JSObject self, int width, int height)
-    /*-{
+	public static native void setGraphicSize(JSObject self, int width, int height)
+	/*-{
 		self.graphicWidth = width;
 		self.graphicHeight = height;
 	}-*/;
-    
-    public static native int getGraphicWidth(JSObject self)
-    /*-{
+
+	public static native int getGraphicWidth(JSObject self)
+	/*-{
 		return self.graphicWidth;
 	}-*/;
-    
-    public static native int getGraphicHeight(JSObject self)
-    /*-{
+
+	public static native int getGraphicHeight(JSObject self)
+	/*-{
 		return self.graphicHeight;
 	}-*/;
-    
-    public static native void setGraphicOffset(JSObject self, int xOffset, int yOffset) 
-    /*-{
+
+	public static native void setGraphicOffset(JSObject self, int xOffset, int yOffset)
+	/*-{
 		self.graphicXOffset = xOffset;
 		self.graphicYOffset = yOffset;
+	}-*/;
+
+	public static native void setBackgroundGraphic(JSObject self, String backgroundGraphic)
+	/*-{
+		self.backgroundGraphic = backgroundGraphic;
+	}-*/;
+
+	public static native String getBackgroundGraphic(JSObject self)
+	/*-{
+		return self.backgroundGraphic;
+	}-*/;
+
+	public static native void setGraphicZIndex(JSObject self, int graphicZIndex)
+	/*-{
+		self.graphicZIndex = graphicZIndex;
+	}-*/;
+
+	public static native int getGraphicZIndex(JSObject self)
+	/*-{
+		return self.graphicZIndex;
+	}-*/;
+
+	public static native void setBackgroundGraphicZIndex(JSObject self, int backgroundGraphicZIndex)
+	/*-{
+		self.backgroundGraphicZIndex = backgroundGraphicZIndex;
+	}-*/;
+
+	public static native int getBackgroundGraphicZIndex(JSObject self)
+	/*-{
+		return self.backgroundGraphicZIndex;
+	}-*/;
+
+	public static native void setBackgroundOffset(JSObject self, int backgroundXOffset, int backgroundYOffset)
+	/*-{
+		self.backgroundXOffset = backgroundXOffset;
+		self.backgroundYOffset = backgroundYOffset;
+	}-*/;
+
+	public static native void setBackgroundWidth(JSObject self, int backgroundWidth)
+	/*-{
+		self.backgroundWidth = backgroundWidth;
+	}-*/;
+
+	public static native int getBackgroundWidth(JSObject self)
+	/*-{
+		return self.backgroundWidth;
+	}-*/;
+
+	public static native void setBackgroundHeight(JSObject self, int backgroundHeight)
+	/*-{
+		self.backgroundHeight = backgroundHeight;
+	}-*/;
+
+	public static native int getBackgroundHeight(JSObject self)
+	/*-{
+		return self.backgroundHeight;
+	}-*/;
+
+	public static native void setBackgroundGraphicSize(JSObject self, int backgroundWidth, int backgroundHeight)
+	/*-{
+		self.backgroundWidth = backgroundWidth;
+		self.backgroundHeight = backgroundHeight;
 	}-*/;
 
 }

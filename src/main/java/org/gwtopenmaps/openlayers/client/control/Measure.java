a9363fa6dc7a982a55b2c6d4cbc2f30a8269c35c
Edwin Commandeur <commandeur.edwin@gmail.com>
2009-09-16 20:58:38 +0200

    use mvn for gwt dependency of main codebase

--- src/main/java/org/gwtopenmaps/openlayers/client/control/Measure.java
+++ src/main/java/org/gwtopenmaps/openlayers/client/control/Measure.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.gwtopenmaps.openlayers.client.control;
 
--- src/main/java/org/gwtopenmaps/openlayers/client/control/MeasureImpl.java
+++ src/main/java/org/gwtopenmaps/openlayers/client/control/MeasureImpl.java
@@ -8,17 +8,17 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 /**
  * @author Michel Vitor A Rodrigues - Intec
- * 
+ *
  */
 public class MeasureImpl {
 
 	public static native JSObject create(JSObject handler)/*-{
-			return new $wnd.OpenLayers.Control.Measure(handler);
-		}-*/;
+		return new $wnd.OpenLayers.Control.Measure(handler);
+	}-*/;
 
-	public static native JSObject create(JSObject handler, JSObject options)/*-{
-			return new $wnd.OpenLayers.Control.Measure(handler, options);
-		}-*/;
+	public static native JSObject create(JSObject handler,JSObject options)/*-{
+		return new $wnd.OpenLayers.Control.Measure(handler, options);
+	}-*/;
 
 	public static native void setPersist(JSObject self, boolean persist)/*-{
 		    self.persist = persist;

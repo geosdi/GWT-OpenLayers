a744e4c2fceccbc2e4e4ae71fc737fffc6c42174
ahhughes <devnull@localhost>
2010-02-12 10:16:19 +1030

    Adding support for XYZ Layer (ESRI Map Cache), ref trac ticket 39

---
+++ src/main/java/org/gwtopenmaps/openlayers/client/layer/XYZImpl.java
@@ -0,0 +1,17 @@
+package org.gwtopenmaps.openlayers.client.layer;
+
+import org.gwtopenmaps.openlayers.client.util.JSObject;
+
+/**
+ * JSNI wrapper to create and wrap access to the OpenLayers.Layer.XYZ JSObject
+ * @author Andrew Hughes
+ * @see XYZ
+ * @see XYZOptions
+ */
+class XYZImpl {
+
+	public static native JSObject create(String name, String url, JSObject options)/*-{
+		return new $wnd.OpenLayers.Layer.XYZ(name, url, options);
+	}-*/;
+	
+}
---
+++ src/main/java/org/gwtopenmaps/openlayers/client/layer/XYZOptions.java
@@ -0,0 +1,19 @@
+package org.gwtopenmaps.openlayers.client.layer;
+
+/**
+ * @author Edwin Commandeur - Atlis EJS
+ * @author Curtis Jensen
+ */
+public class XYZOptions extends GridLayerOptions {
+
+	/**
+	 * 
+	 * @param b
+	 */
+	public void setSphericalMercator(boolean b){
+		getJSObject().setProperty("sphericalMercator", b);
+	}
+	
+	
+
+}

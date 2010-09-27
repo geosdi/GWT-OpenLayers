c7e680b42c552f4199bfdc7d0f874ba556359e34
Edwin Commandeur <commandeur.edwin@gmail.com>
2010-09-27 22:27:51 +0200

    patch for WMSGetFeatureInfo with several enhancements, from Lukas Johansson

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/control/WMSGetFeatureInfoOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/control/WMSGetFeatureInfoOptions.java
@@ -1,13 +1,15 @@
 package org.gwtopenmaps.openlayers.client.control;
 
+import org.gwtopenmaps.openlayers.client.layer.WMS;
+import org.gwtopenmaps.openlayers.client.util.JObjectArray;
 import org.gwtopenmaps.openlayers.client.util.JSObject;
 import org.gwtopenmaps.openlayers.client.util.JSObjectWrapper;
+import org.gwtopenmaps.openlayers.client.util.JStringArray;
 
 
 /**
- *
  * @author Dave Potts
- *
+ * @author Lukas Johansson
  */
 public class WMSGetFeatureInfoOptions extends JSObjectWrapper {
 
@@ -35,6 +37,11 @@ public class WMSGetFeatureInfoOptions extends JSObjectWrapper {
 		return getJSObject().getPropertyAsInt("maxFeatures");
 	}
 
+	/**
+	 * The URL of the WMS service to use.  If not provided, the url
+	 * of the first eligible layer will be used.
+	 * @param url The url to which the WmsGetFeatureInfo will be called against
+	 */
 	public void setURL(String url){
 		getJSObject().setProperty("url", url);
 	}
@@ -47,11 +54,47 @@ public class WMSGetFeatureInfoOptions extends JSObjectWrapper {
 		getJSObject().setProperty("queryVisible", qvis);
 	}
 
-//	public void setLayers(WMS[] layers){
-//
-//	}
+	/**
+ 	 * The layers to query for feature info.
+ 	 * If omitted, all map WMS layers with a url that matches this <url> or
+ 	 * <layerUrls> will be considered.
+	 * @param layers The layers to query
+	 */
+	public void setLayers(WMS[] layers){
+		JObjectArray jsArray = JObjectArray.narrowToJObjectArray(JSObject.createJSArray());
+		for (int i = 0; layers != null && i < layers.length; i++) {
+			jsArray.set(i, layers[i].getJSObject());
+	    }
+		getJSObject().setProperty("layers", jsArray.getJSObject());
+	}
 
 	/**
+	 * Optional list of urls for layers that should be queried.
+	 * This can be used when the layer url differs from the url used for
+	 * making GetFeatureInfo requests (in the case of a layer using cached
+	 * tiles).
+	 *
+	 * Notice that this will not cause a WmsGetFeatureInfo request to be sent
+	 * to all urls, its merely to include layers that don't match this.url.
+	 * For a a request to be sent to each url the propery drillDown must be set
+	 * to true.
+	 * @param urls urls that should be considered
+	 */
+	public void setLayerUrls(String[] urls){
+		getJSObject().setProperty("layerUrls", new JStringArray(urls).getJSObject());
+	}
+	/**
+	 * Drill down over all WMS layers in the map. When
+     * using drillDown mode, hover is not possible, and an infoFormat that
+	 * returns parseable features is required. Default is false.
+	 * @param value indicates whether drillDown should be enabled or not
+	 */
+	public void setDrillDown(boolean value){
+		getJSObject().setProperty("drillDown", value);
+	}
+
+	/**
+	 * The mimetype to request from the server
 	 * defaults to 'text/html'
 	 */
 	public void setInfoFormat(String infoFormat){
--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/event/GetFeatureInfoListener.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/event/GetFeatureInfoListener.java
@@ -1,13 +1,14 @@
 package org.gwtopenmaps.openlayers.client.event;
 
 import org.gwtopenmaps.openlayers.client.control.WMSGetFeatureInfo;
+import org.gwtopenmaps.openlayers.client.feature.VectorFeature;
 import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 
 
 /**
  * @author Dave Potts
- *
+ * @author Lukas Johansson
  */
 public interface GetFeatureInfoListener extends EventListener {
 
@@ -21,10 +22,34 @@ public interface GetFeatureInfoListener extends EventListener {
 			JSObject object = getSourceJSObject();
 			return (object!=null)?WMSGetFeatureInfo.narrowToWMSGetFeatureInfo(object):null;
 		}
+
 		public String getText(){
 			return getJSObject().getPropertyAsString("text");
-
 		}
+
+		/**
+		 * Get the features returned by the request
+		 * If infoFormat was set to GML WmsGetFeatureInfo parses the result and populates
+		 * an array with the returned features. This method simple returns that array
+		 * @return array of features
+		 */
+		public VectorFeature[] getFeatures(){
+			JSObject[] jsArr = getJSObject().getPropertyAsArray("features");
+			if(jsArr.length == 0){
+				return null;
+			}
+			//In some cases the parser seems to produces an array with length=1 but with null as element
+			if(jsArr[0] == null){
+				return null;
+			}
+			VectorFeature[] result = new VectorFeature[jsArr.length];
+			for(int i = 0; i < jsArr.length; i++){
+				VectorFeature vf = VectorFeature.narrowToVectorFeature(jsArr[i]);
+				result[i] = vf;
+			}
+			return result;
+		}
+
 	}
 
 	public void onGetFeatureInfo(GetFeatureInfoEvent eventObject);

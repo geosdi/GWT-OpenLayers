b3f43bb5131c13dceedd1eca7da5896b236a238a
Pinocchio <devnull@localhost>
2011-04-21 11:41:20 +0200

    SymbolizerText

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/style/SymbolizerRaster.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/style/SymbolizerRaster.java
@@ -2,6 +2,11 @@ package org.gwtopenmaps.openlayers.client.style;
 
 import org.gwtopenmaps.openlayers.client.util.JSObject;
 
+/**
+ * 
+ * @author Maciej Jezierski - Pinocchio
+ * A symbolizer used to render text labels for features.
+ */
 public class SymbolizerRaster extends Symbolizer{
 
 	public SymbolizerRaster(JSObject config) {

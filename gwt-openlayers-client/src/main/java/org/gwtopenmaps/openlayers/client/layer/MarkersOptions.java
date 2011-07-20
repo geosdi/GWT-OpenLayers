875e65837124b32c5d9e939425a8f9f7cf4b1d80
Giuseppe La Scaleia <glascaleia@gmail.com>
2011-07-20 15:10:12 +0200

    Some Improvements to MarkersOptions

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/MarkersOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/MarkersOptions.java
@@ -1,30 +1,28 @@
 package org.gwtopenmaps.openlayers.client.layer;
 
 import org.gwtopenmaps.openlayers.client.util.JSObject;
-import org.gwtopenmaps.openlayers.client.util.JSObjectWrapper;
 
 /**
  *
  * @author Edwin Commandeur - Atlis EJS
  *
  */
-public class MarkersOptions extends JSObjectWrapper {
+public class MarkersOptions extends LayerOptions {
 
-	protected MarkersOptions(JSObject jsObject) {
-		super(jsObject);
-	}
+    protected MarkersOptions(JSObject jsObject) {
+        super(jsObject);
+    }
 
-	public MarkersOptions(){
-		this(JSObject.createJSObject());
-	}
-	
-	/**
-	 * 
-	 * @param maxZoomLevel
-	 */
-	public void setMaxZoomLevel(int maxZoomLevel) {
-		getJSObject().setProperty("maxZoomLevel", maxZoomLevel);
-	}
+    public MarkersOptions() {
+        this(JSObject.createJSObject());
+    }
 
-	// What kind of options has Markers?
+    /**
+     * 
+     * @param maxZoomLevel
+     */
+    public void setMaxZoomLevel(int maxZoomLevel) {
+        getJSObject().setProperty("maxZoomLevel", maxZoomLevel);
+    }
+    // What kind of options has Markers?
 }

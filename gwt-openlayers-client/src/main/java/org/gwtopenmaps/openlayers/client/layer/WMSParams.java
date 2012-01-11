299b2a422f4fe9ba2c2a3028cc757fc5eb69bc48
glascaleia <glascaleia@MacBook-Pro-di-Giuseppe.local>
2012-01-11 12:07:41 +0100

    Test for email sending

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/WMSParams.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/WMSParams.java
@@ -16,61 +16,65 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
  */
 public class WMSParams extends Params {
 
-	protected WMSParams(JSObject jsObject) {
-		super(jsObject);
-	}
+    protected WMSParams(JSObject jsObject) {
+        super(jsObject);
+    }
 
-	public WMSParams(){
-		this(JSObject.createJSObject());
-	}
+    public WMSParams() {
+        this(JSObject.createJSObject());
+    }
 
-	public void setLayers(String layers) {
-		getJSObject().setProperty("layers", layers);
-	}
-	public String getLayers() {
-		return getJSObject().getPropertyAsString("layers");
-	}
+    public void setLayers(String layers) {
+        getJSObject().setProperty("layers", layers);
+    }
 
-	public void setStyles(String styles) {
-		getJSObject().setProperty("styles", styles);
-	}
-	public String getStyles() {
-		return getJSObject().getPropertyAsString("styles");
-	}
+    public String getLayers() {
+        return getJSObject().getPropertyAsString("layers");
+    }
 
-	public void setFormat(String styles) {
-		getJSObject().setProperty("format", styles);
-	}
-	public String getFormat() {
-		return getJSObject().getPropertyAsString("format");
-	}
+    public void setStyles(String styles) {
+        getJSObject().setProperty("styles", styles);
+    }
 
-	public void setMaxExtent(Bounds bounds) {
-		getJSObject().setProperty("maxExtent", bounds.getJSObject());
-	}
-	public Bounds getMaxExtent() {
-		return Bounds.narrowToBounds(getJSObject().getProperty("maxExtent"));
-	}
+    public String getStyles() {
+        return getJSObject().getPropertyAsString("styles");
+    }
 
-	/**
-	 * Set transparent parameter in WMS request. Default is false.
-	 *
-	 * Within this method the value of the parameter will be set in uppercase,
-	 * since at least IONIC's WMS requires it to be in uppercase, in
-	 * accordance with the WMS Specification
-	 *
-	 * The WMS 1.1.1 spec states:
-	 *  "TRANSPARENT can take on two values, "TRUE" or "FALSE"."
-	 *  and
-	 *  "Parameter names shall not be case sensitive,
-	 *  but parameter values shall be case sensitive."
-	 *
-	 */
-	public void setIsTransparent(boolean isTransparent){
-		if(isTransparent){
-			getJSObject().setProperty("transparent", "TRUE");
-		} else {
-			getJSObject().setProperty("transparent", "FALSE");
-		}
-	}
+    public void setFormat(String styles) {
+        getJSObject().setProperty("format", styles);
+    }
+
+    public String getFormat() {
+        return getJSObject().getPropertyAsString("format");
+    }
+
+    public void setMaxExtent(Bounds bounds) {
+        getJSObject().setProperty("maxExtent", bounds.getJSObject());
+    }
+
+    public Bounds getMaxExtent() {
+        return Bounds.narrowToBounds(getJSObject().getProperty("maxExtent"));
+    }
+
+    /**
+     * Set transparent parameter in WMS request. Default is false.
+     *
+     * Within this method the value of the parameter will be set in uppercase,
+     * since at least IONIC's WMS requires it to be in uppercase, in
+     * accordance with the WMS Specification
+     *
+     * The WMS 1.1.1 spec states:
+     *  "TRANSPARENT can take on two values, "TRUE" or "FALSE"."
+     *  and
+     *  "Parameter names shall not be case sensitive,
+     *  but parameter values shall be case sensitive."
+     *
+     */
+    public void setIsTransparent(boolean isTransparent) {
+        if (isTransparent) {
+            getJSObject().setProperty("transparent", "TRUE");
+        } else {
+            getJSObject().setProperty("transparent", "FALSE");
+        }
+    }
 }

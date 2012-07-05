41efb3e7f0d969a72504b3d9bbdbb4d6d1e89d4e
fwynants <devnull@localhost>
2012-07-05 08:04:33 +0200

    Disabled Google V3 examples as V2, and V3 can't co-exist in one project. Since V3 is discouraged for production use V3 got disabled.

--- gwt-openlayers-showcase/src/main/java/org/gwtopenmaps/demo/openlayers/client/examples/basicgooglev3/BasicGoogleV3Example.java
+++ gwt-openlayers-showcase/src/main/java/org/gwtopenmaps/demo/openlayers/client/examples/basicgooglev3/BasicGoogleV3Example.java
@@ -2,7 +2,7 @@ package org.gwtopenmaps.demo.openlayers.client.examples.basicgooglev3;
 
 import org.gwtopenmaps.demo.openlayers.client.InfoPanel;
 import org.gwtopenmaps.demo.openlayers.client.examples.AbstractExample;
-import org.gwtopenmaps.openlayers.client.LonLat;
+/*import org.gwtopenmaps.openlayers.client.LonLat;
 import org.gwtopenmaps.openlayers.client.Map;
 import org.gwtopenmaps.openlayers.client.MapOptions;
 import org.gwtopenmaps.openlayers.client.MapWidget;
@@ -12,13 +12,13 @@ import org.gwtopenmaps.openlayers.client.control.OverviewMap;
 import org.gwtopenmaps.openlayers.client.control.ScaleLine;
 import org.gwtopenmaps.openlayers.client.layer.GoogleV3;
 import org.gwtopenmaps.openlayers.client.layer.GoogleV3MapType;
-import org.gwtopenmaps.openlayers.client.layer.GoogleV3Options;
+import org.gwtopenmaps.openlayers.client.layer.GoogleV3Options;*/
 
 import com.google.gwt.user.client.ui.HTML;
 
 public class BasicGoogleV3Example extends AbstractExample
 {
-   private static final Projection DEFAULT_PROJECTION = new Projection("EPSG:4326");
+   //private static final Projection DEFAULT_PROJECTION = new Projection("EPSG:4326");
 
    /**
     * Constructor.
@@ -33,6 +33,7 @@ public class BasicGoogleV3Example extends AbstractExample
    @Override
    public void buildPanel()
    {
+       /*
       //create some MapOptions
       MapOptions defaultMapOptions = new MapOptions();
       defaultMapOptions.setNumZoomLevels(16);
@@ -76,15 +77,16 @@ public class BasicGoogleV3Example extends AbstractExample
       //Center and zoom to a location
       LonLat lonLat = new LonLat(6.95, 50.94);
       lonLat.transform(DEFAULT_PROJECTION.getProjectionCode() , map.getProjection()); //transform lonlat to OSM coordinate system
-      map.setCenter(lonLat, 12);
+      map.setCenter(lonLat, 12);*/
 
       contentPanel.add(new HTML("<p>This example shows how to add Google V3 layers to GWT-OL.</p><p>If you use the Google Maps v3 API with a Google layer, you don't need to include an API key. This layer only works in the spherical mercator projection.</p><p><b>Note on Google Maps API versioning:</b> This example uses the \"nightly\" version of Google Maps API. This is specified by using v=3 in the the Google Maps API URL. Production applications should use the \"release\" or \"frozen\" versions of Google Maps API. See the OpenLayers.Layer.Google.v3 API docs, and the Versioning Section of the Google Maps API docs, for more details.</p>"));
       contentPanel.add(new InfoPanel("<p>Don't forget to add the following line to your HTML if you want to use Google V3. :</p>" +
       		"<p><b>&lt;script src=\"http://maps.google.com/maps/api/js?v=3&amp;sensor=false\"&gt;&lt;/script&gt;</b></p>"));
-      contentPanel.add(mapWidget);
+      contentPanel.add(new InfoPanel("<p>Note that this example is not working in this showcase. This is because the Google V2 and V3 API's conflict with each other. If you however look at the source, and create a project only containing Google V3 it should work without a problem.</b></p>"));
+      //contentPanel.add(mapWidget);
 
       initWidget(contentPanel);
 
-      mapWidget.getElement().getFirstChildElement().getStyle().setZIndex(0); //force the map to fall behind popups
+      //mapWidget.getElement().getFirstChildElement().getStyle().setZIndex(0); //force the map to fall behind popups
    }
 }

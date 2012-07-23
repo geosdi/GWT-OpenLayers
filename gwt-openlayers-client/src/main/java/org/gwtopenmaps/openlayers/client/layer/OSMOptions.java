bcc239e0628c866cd1fccf30718ddc055c340fc6
jonb <jonb@postel>
2012-07-23 15:54:06 +0100

    OSMOptions now extends XYZOptions, providing access to properties such as zoomOffset.

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/OSMOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/OSMOptions.java
@@ -1,16 +1,11 @@
 package org.gwtopenmaps.openlayers.client.layer;
 
-public class OSMOptions extends LayerOptions
+public class OSMOptions extends XYZOptions
 {
 
     public OSMOptions()
     {
         //
-    }
-
-    public void setDisplayOutsideMaxExtent(boolean value)
-    {
-        getJSObject().setProperty("displayOutsideMaxExtent", value);
     }
 
     public void setWrapDateLine(boolean value)

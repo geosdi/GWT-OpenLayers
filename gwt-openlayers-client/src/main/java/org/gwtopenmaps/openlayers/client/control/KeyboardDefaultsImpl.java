ae54e7019c26d0d21f2c5b0f462cc6b715c6ffff
Todor Todorov <todor@peychev.com>
2012-10-17 16:18:06 +0200

    KeyboardDefaults wrappers, found in http://sourceforge.net/apps/trac/gwt-openlayers/ticket/65

---
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/control/KeyboardDefaultsImpl.java
@@ -0,0 +1,17 @@
+package org.gwtopenmaps.openlayers.client.control;
+
+import org.gwtopenmaps.openlayers.client.util.JSObject;
+
+/**
+ * 
+ *
+ * @author Martin Hultman - Swedish Meteorological and Hydrological Institute
+ *
+ */
+public class KeyboardDefaultsImpl {
+
+	public static native JSObject create()/*-{
+		return new $wnd.OpenLayers.Control.KeyboardDefaults();
+	}-*/;
+
+}

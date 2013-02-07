17a211254684d53fefcd5c7085512b7c1f9dfc38
fwynants <devnull@localhost>
2013-02-07 09:09:52 +0100

    Added setMaxGetUrlLength to TileOptions to specify a maximum URL length. When the URL is longer than the specified value, a POST is issued.

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/tile/TileOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/tile/TileOptions.java
@@ -5,7 +5,7 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 /**
  * @author Niels Meijer
- * 
+ *
  */
 public class TileOptions extends Params {
 
@@ -17,6 +17,13 @@ public class TileOptions extends Params {
 		this(JSObject.createJSObject());
 	}
 
-	
-
+	/**
+	 * If set, requests that would result in GET urls with more characters than the number provided will be made using form-encoded HTTP POST.
+	 * It is good practice to avoid urls that are longer than 2048 characters.
+	 * @param maxGetUrlLength The max length of an URL, otherwise a POST is used.
+	 */
+    public void setMaxGetUrlLength(int maxGetUrlLength)
+    {
+        setParameter("maxGetUrlLength", Integer.toString(maxGetUrlLength));
+    }
 }

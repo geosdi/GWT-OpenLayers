c42079d7df4ca02fb63dfb39662b48b27ff95c02
fwynants <devnull@localhost>
2013-02-07 09:26:58 +0100

    Added setStyleMap method to PathHandlerOptions
    This fixes issue #13 (https://bitbucket.org/gwtopenlayers/gwt-openlayers/issue/13/cant-set-handleroptions-stylemap-in)

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/handler/PathHandlerOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/handler/PathHandlerOptions.java
@@ -1,5 +1,6 @@
 package org.gwtopenmaps.openlayers.client.handler;
 
+import org.gwtopenmaps.openlayers.client.StyleMap;
 import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 
@@ -19,5 +20,16 @@ public class PathHandlerOptions extends HandlerOptions
     public PathHandlerOptions()
     {
         this(JSObject.createJSObject());
+    }
+
+    /**
+     * Set the stylemap to use.
+     * @param styleMap The stylemap to use.
+     */
+    public void setStyleMap(StyleMap styleMap)
+    {
+        JSObject styleOption = JSObject.createJSObject();
+        styleOption.setProperty("styleMap", styleMap.getJSObject());
+        this.getJSObject().setProperty("layerOptions", styleOption);
     }
 }

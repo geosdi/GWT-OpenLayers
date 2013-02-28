f1bffbbb4c14a76c2dd8aac11195ef723d61ca91
Sandor Forrai <devnull@localhost>
2013-02-28 01:06:41 +0100

    Implemented Marker#moveTo() (and some code formatting)

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/Marker.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/Marker.java
@@ -1,19 +1,3 @@
-/**
- *
- *   Copyright 2013 sourceforge.
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
 package org.gwtopenmaps.openlayers.client;
 
 import org.gwtopenmaps.openlayers.client.event.EventHandler;
@@ -24,67 +8,68 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 
 /**
- *
  * @author Erdem Gunay
  * @author Michel Vitor - Intec
- *
+ * @author Sandor Forrai
  */
 public class Marker extends OpenLayersEObjectWrapper {
-    
+
 	protected Marker(JSObject element) {
 		super(element);
 	}
 
-	public static Marker narrowToMarker(JSObject element)
-	{
-		return (element == null)? null: new Marker(element);
-	}
 	public Marker(LonLat lonlat) {
-		this (MarkerImpl.create(lonlat.getJSObject()));
+		this(MarkerImpl.create(lonlat.getJSObject()));
 	}
 
 	public Marker(LonLat lonlat, Icon icon) {
-		this (MarkerImpl.create(lonlat.getJSObject(), icon.getJSObject()));
+		this(MarkerImpl.create(lonlat.getJSObject(), icon.getJSObject()));
+	}
+
+	public static Marker narrowToMarker(JSObject element) {
+		return (element == null) ? null : new Marker(element);
 	}
 
 	public LonLat getLonLat() {
 		return LonLat.narrowToLonLat(MarkerImpl.getLonLat(getJSObject()));
 	}
 
+	/**
+	 * Method setLonlat Change the lonlat of Icon lonlat (LonLat) the position of this marker
+	 */
+	public void setLonLat(LonLat lonlat) {
+		MarkerImpl.setLonLat(getJSObject(), lonlat.getJSObject());
+	}
+
 	public Icon getIcon() {
 		return Icon.narrowToIcon(MarkerImpl.getIcon(getJSObject()));
 	}
-   /**
-	* Method: setIcon
-	* Change the icon image.
-	* icon (Icon) the image of icon  
-	*/    
-    public void setIcon(Icon icon){
-        MarkerImpl.setIcon(getJSObject(),icon.getJSObject()); 	
-    }
-    /**
-	 * Method setLonlat
-	 * Change the lonlat of Icon
-	 * lonlat (LonLat) the position of this marker
+
+	/**
+	 * Method: setIcon Change the icon image. icon (Icon) the image of icon
 	 */
-    public void setLonLat(LonLat lonlat){
-    	MarkerImpl.setLonLat(getJSObject(),lonlat.getJSObject());    	
-    }
-    /**
-	 * Method setImageUrl
-	 * Change the Url of Icon
-	 * imageUrl (Url) the url of this marker
+	public void setIcon(Icon icon) {
+		MarkerImpl.setIcon(getJSObject(), icon.getJSObject());
+	}
+
+	/**
+	 * Method setImageUrl Change the Url of Icon imageUrl (Url) the url of this marker
 	 */
-    public void setImageUrl(String imageUrl){
-    	MarkerImpl.setImageUrl(getJSObject(),imageUrl);	
-    }
-	public void addBrowserEventListener(String browserEvent, final MarkerBrowserEventListener listener){
-		eventListeners.addListener(this, listener, browserEvent, new EventHandler (){
+	public void setImageUrl(String imageUrl) {
+		MarkerImpl.setImageUrl(getJSObject(), imageUrl);
+	}
+
+	public void addBrowserEventListener(String browserEvent, final MarkerBrowserEventListener listener) {
+		eventListeners.addListener(this, listener, browserEvent, new EventHandler() {
 			public void onHandle(EventObject eventObject) {
 				MarkerBrowserEvent e = new MarkerBrowserEvent(eventObject);
 				listener.onBrowserEvent(e);
 			}
 		});
-	};
+	}
 
+	public void moveTo(Pixel where) {
+		MarkerImpl.moveTo(getJSObject(), where.getJSObject());
+	}
 }
+
--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/MarkerImpl.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/MarkerImpl.java
@@ -1,53 +1,47 @@
-/**
- *
- *   Copyright 2013 sourceforge.
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
 package org.gwtopenmaps.openlayers.client;
 
 import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 
 /**
- * 
  * @author Erdem Gunay
  * @author Michel Vitor - Intec
+ * @author Sandor Forrai
  */
 class MarkerImpl {
 
-	
+
 	public static native JSObject create(JSObject lonlat)/*-{
-		return new $wnd.OpenLayers.Marker(lonlat);
-	}-*/;
-	public static native void setIcon(JSObject self,JSObject icon)/*-{
-	     self.icon = icon;
-	     self.events= new $wnd.OpenLayers.Events(self,self.icon.imageDiv,null);     
+        return new $wnd.OpenLayers.Marker(lonlat);
     }-*/;
-	public static native void setLonLat(JSObject self,JSObject lonlat)/*-{
+
+	public static native void setIcon(JSObject self, JSObject icon)/*-{
+        self.icon = icon;
+        self.events = new $wnd.OpenLayers.Events(self, self.icon.imageDiv, null);
+    }-*/;
+
+	public static native void setLonLat(JSObject self, JSObject lonlat)/*-{
         self.lonlat = lonlat;
     }-*/;
+
 	public static native JSObject create(JSObject lonlat, JSObject icon)/*-{
-		return new $wnd.OpenLayers.Marker(lonlat, icon);
-	}-*/;
+        return new $wnd.OpenLayers.Marker(lonlat, icon);
+    }-*/;
 
 	public static native JSObject getLonLat(JSObject self)/*-{
-		return self.lonlat;
-	}-*/;
-	public static native void setImageUrl(JSObject self, String imageUrl)/*-{
-          self.icon.url = imageUrl;
+        return self.lonlat;
     }-*/;
+
+	public static native void setImageUrl(JSObject self, String imageUrl)/*-{
+        self.icon.url = imageUrl;
+    }-*/;
+
 	public static native JSObject getIcon(JSObject self)/*-{
-		return self.icon;
-	}-*/;
+        return self.icon;
+    }-*/;
+
+	public static native void moveTo(JSObject self, JSObject where)/*-{
+        self.moveTo(where);
+    }-*/;
 }
+

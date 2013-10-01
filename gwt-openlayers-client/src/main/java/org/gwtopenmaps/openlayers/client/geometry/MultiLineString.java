d2f3b59bea7f36175f916d938aba58c955ae3c3f
Giuseppe La Scaleia <glascaleia@gmail.com>
2013-10-01 16:01:34 +0200

    Some Improvements

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/geometry/MultiLineString.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/geometry/MultiLineString.java
@@ -1,45 +1,41 @@
 /**
  *
- *   Copyright 2013 sourceforge.
+ * Copyright 2013 sourceforge.
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.gwtopenmaps.openlayers.client.geometry;
 
 import org.gwtopenmaps.openlayers.client.util.JObjectArray;
 import org.gwtopenmaps.openlayers.client.util.JSObject;
 
-
 /**
- * @author giuseppe
  *
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
  */
-public class MultiLineString extends Collection
-{
+public class MultiLineString extends Collection {
 
-    public static MultiLineString narrowToMultiLineString(JSObject multiLine)
-    {
+    public static MultiLineString narrowToMultiLineString(JSObject multiLine) {
         return (multiLine == null) ? null : new MultiLineString(multiLine);
     }
 
-    protected MultiLineString(JSObject element)
-    {
+    protected MultiLineString(JSObject element) {
         super(element);
         // TODO Auto-generated constructor stub
     }
 
-    public MultiLineString(LineString[] lines)
-    {
+    public MultiLineString(LineString[] lines) {
         this(MultiLineStringImpl.create((new JObjectArray(lines)).getJSObject()));
     }
 

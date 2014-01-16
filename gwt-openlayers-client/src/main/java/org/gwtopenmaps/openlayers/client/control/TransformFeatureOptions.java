1dc293693d123c22136b31a8c9a9623815bcc0fb
Frank Wynants <frank.wynants@fks.be>
2014-01-16 15:36:45 +0100

    Added setPreserveAspectRatio to TransformFeatureOptions
    This gives the option to keep the same aspect ratio when resizing a
    feature using a TransformFeature.
    
    Also some missing and wrong javadocs where fixed in
    TransformFeatureOptions

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/control/TransformFeatureOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/control/TransformFeatureOptions.java
@@ -17,7 +17,6 @@
 package org.gwtopenmaps.openlayers.client.control;
 
 import org.gwtopenmaps.openlayers.client.Style;
-import org.gwtopenmaps.openlayers.client.feature.VectorFeature;
 
 
 /**
@@ -26,50 +25,54 @@ import org.gwtopenmaps.openlayers.client.feature.VectorFeature;
 */
 
 public class TransformFeatureOptions extends ControlOptions{
-	
+
 	/**
      * Set the current rotation angle of the box
-     * 
      * @param rotation
      */
-	
 	public void setRotation (int rotation){
 		 getJSObject().setProperty("rotation", rotation);
 	}
-	
-	/** Set the scale of the feature, relative to the scale the time the feature was set.
-	 * 
+
+	/**
+	 * Set the scale of the feature, relative to the scale the time the feature was set.
 	 * @param scale
 	 */
-	
+
 	public void setScale (float scale){
 		 getJSObject().setProperty("scale", scale);
 	}
-        
-        
-        /** Set the scale of the feature, relative to the scale the time the feature was set.
-	 * 
-	 * @param rotate
-	 */
-        public void setRotate (boolean rotate){
-		 getJSObject().setProperty("rotate", rotate);
-	}
-        
-        /** Set the Render intent for the transformation box and handles.
-	 * 
-	 * @param intent
-	 */
-        public void setRederIntent (String intent){
-         getJSObject().setProperty("renderIntent", intent);
-        }
-        
-        public void setRotationHandleSymbolizer(Style style){
-         getJSObject().setProperty("rotationHandleSymbolizer", style.getJSObject());
-        }
-        
-//        public void setFeature (VectorFeature feature){
-//        getJSObject().setProperty("feature", feature.getJSObject());
-//        }
-	
+
+
+    /**
+     * Set to false if rotation should be disabled.  Default is true.
+     * @param rotate
+     */
+    public void setRotate (boolean rotate){
+        getJSObject().setProperty("rotate", rotate);
+    }
+
+    /** Set the Render intent for the transformation box and handles.
+     *  @param intent
+     */
+    public void setRederIntent (String intent){
+        getJSObject().setProperty("renderIntent", intent);
+    }
+
+    /**
+     * Custom symbolizer for the rotation handles.
+     * @param style the style for the rotation handlers
+     */
+    public void setRotationHandleSymbolizer(Style style){
+        getJSObject().setProperty("rotationHandleSymbolizer", style.getJSObject());
+    }
+
+    /**
+     * Set to true to not change the feature’s aspect ratio, default is false.
+     * @param preserveAspectRatio True or false
+     */
+    public void setPreserveAspectRatio(boolean preserveAspectRatio) {
+        getJSObject().setProperty("preserveAspectRatio", preserveAspectRatio);
+    }
 
 }

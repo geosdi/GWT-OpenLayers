d72a4a8ab01591e02f9330d181eef46eee4454a5
Giuseppe La Scaleia <glascaleia@gmail.com>
2014-05-07 15:16:24 +0200

    Add @Depracated for Canvas Layer

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/Canvas.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/Canvas.java
@@ -1,24 +1,23 @@
 /**
  *
- *   Copyright 2014 sourceforge.
+ * Copyright 2014 sourceforge.
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.gwtopenmaps.openlayers.client.layer;
 
 import org.gwtopenmaps.openlayers.client.LonLat;
 import org.gwtopenmaps.openlayers.client.util.JSObject;
-
 
 /**
  *
@@ -27,21 +26,19 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
  * @author Erdem Gunay
  *
  */
-public class Canvas extends Layer
-{
+@Deprecated
+public class Canvas extends Layer {
 
-    protected Canvas(JSObject element)
-    {
+    protected Canvas(JSObject element) {
         super(element);
     }
 
-    public Canvas(String name)
-    {
+    public Canvas(String name) {
         this(CanvasImpl.create(name));
     }
 
-    public void drawLine(LonLat p1, LonLat p2)
-    {
+    public void drawLine(LonLat p1, LonLat p2) {
         CanvasImpl.drawLine(getJSObject(), p1.getJSObject(), p2.getJSObject());
     }
+
 }

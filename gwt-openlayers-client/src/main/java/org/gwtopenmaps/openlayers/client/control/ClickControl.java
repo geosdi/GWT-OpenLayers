b011fdfeded79f628685eff7fb3422d212e5ba3a
Giuseppe La Scaleia <glascaleia@gmail.com>
2014-06-06 15:21:15 +0200

    Move ClickControl in gwt-openlayers-client module

---
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/control/ClickControl.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014 geoSDI.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gwtopenmaps.openlayers.client.control;
+
+import org.gwtopenmaps.openlayers.client.handler.ClickHandler;
+import org.gwtopenmaps.openlayers.client.handler.ClickHandlerOptions;
+
+/**
+ * Custom control for handling click events.
+ *
+ * @author Tino Desjardins - SRP
+ */
+public class ClickControl extends Control {
+
+    private ClickHandler clickHandler;
+    private final ClickHandlerOptions clickHandlerOptions;
+
+    public ClickControl(ClickHandlerOptions clickHandlerOptions) {
+        this.clickHandlerOptions = clickHandlerOptions;
+    }
+
+    @Override
+    public boolean activate() {
+
+        if (this.getMap() != null) {
+
+            if (this.clickHandler == null) {
+                this.clickHandler = new ClickHandler(this, this.clickHandlerOptions);
+            }
+
+            this.clickHandler.activate();
+
+        }
+
+        return super.activate();
+    }
+
+    @Override
+    public boolean deactivate() {
+
+        if (this.clickHandler != null) {
+            this.clickHandler.deactivate();
+        }
+
+        return super.deactivate();
+    }
+
+}

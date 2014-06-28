7c9e287f1322380e13f1e2ec252ca952e1a39107
Tino Desjardins <tino.desjardins@arcor.de>
2014-06-28 13:58:32 +0200

    Extended PolygonHandler by PolygonHandlerOptions for drawing holes in polygons

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/handler/PathHandlerOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/handler/PathHandlerOptions.java
@@ -21,20 +21,17 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 
 /**
- * Options which control the behavior of the RegularPolygonHandler
+ * Options which control the behavior of the RegularPolygonHandler.
  * @author rhs - Roland.Schweitzer@noaa.gov
  *
  */
-public class PathHandlerOptions extends HandlerOptions
-{
+public class PathHandlerOptions extends HandlerOptions {
 
-    protected PathHandlerOptions(JSObject jsObject)
-    {
+    protected PathHandlerOptions(JSObject jsObject) {
         super(jsObject);
     }
 
-    public PathHandlerOptions()
-    {
+    public PathHandlerOptions() {
         this(JSObject.createJSObject());
     }
 
@@ -42,10 +39,10 @@ public class PathHandlerOptions extends HandlerOptions
      * Set the stylemap to use.
      * @param styleMap The stylemap to use.
      */
-    public void setStyleMap(StyleMap styleMap)
-    {
+    public void setStyleMap(StyleMap styleMap) {
         JSObject styleOption = JSObject.createJSObject();
         styleOption.setProperty("styleMap", styleMap.getJSObject());
         this.getJSObject().setProperty("layerOptions", styleOption);
     }
+    
 }

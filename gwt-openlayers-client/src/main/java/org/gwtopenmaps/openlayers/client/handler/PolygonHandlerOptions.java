0ccb40b2b52218678a32870d350d034d62792214
Giuseppe La Scaleia <glascaleia@gmail.com>
2014-06-28 15:18:27 +0200

    Some Improvements

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/handler/PolygonHandlerOptions.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/handler/PolygonHandlerOptions.java
@@ -1,18 +1,18 @@
 /**
  *
- *   Copyright 2014 sourceforge.
+ * Copyright 2014 sourceforge.
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.gwtopenmaps.openlayers.client.handler;
 
@@ -27,24 +27,25 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
 public class PolygonHandlerOptions extends PathHandlerOptions {
 
     public enum HoleModifier {
-		altKey,
-		shiftKey,
-		ctrlKey
-	}
 
-	protected PolygonHandlerOptions(JSObject jsObject) {
-		super(jsObject);
-	}
+        altKey,
+        shiftKey,
+        ctrlKey
+    }
 
-	public PolygonHandlerOptions() { 
-		this(JSObject.createJSObject());
-	}
+    protected PolygonHandlerOptions(JSObject jsObject) {
+        super(jsObject);
+    }
 
-	public void setHoleModifier(HoleModifier holeModifier) {
+    public PolygonHandlerOptions() {
+        this(JSObject.createJSObject());
+    }
 
-		if (holeModifier != null) {
-		    getJSObject().setProperty("holeModifier", holeModifier.toString());
-		}
-	}
+    public void setHoleModifier(HoleModifier holeModifier) {
+
+        if (holeModifier != null) {
+            getJSObject().setProperty("holeModifier", holeModifier.toString());
+        }
+    }
 
 }

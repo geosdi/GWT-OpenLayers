dd80f29e4ba70b0122c8106666fb1158e900f1b8
Giuseppe La Scaleia <glascaleia@gmail.com>
2014-09-17 12:34:03 +0200

    All Impl Classes must have private access

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/GridLayerImpl.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/GridLayerImpl.java
@@ -1,18 +1,18 @@
 /**
  *
- *   Copyright 2014 sourceforge.
+ * Copyright 2014 sourceforge.
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.gwtopenmaps.openlayers.client.layer;
 
@@ -20,42 +20,42 @@ import org.gwtopenmaps.openlayers.client.util.JSObject;
 
 /**
  * @author Nazzareno Sileno - CNR IMAA geoSDI Group
- * @email nazzareno.sileno@geosdi.org 
+ * @email nazzareno.sileno@geosdi.org
  */
-public class GridLayerImpl
-{
+class GridLayerImpl {
 
     public static native JSObject create(String name, String url,
-        JSObject params, JSObject options) /*-{
-        return new $wnd.OpenLayers.Layer.Grid(name, url, params,options);
-    }-*/;
+            JSObject params, JSObject options) /*-{
+     return new $wnd.OpenLayers.Layer.Grid(name, url, params,options);
+     }-*/;
 
     public static native JSObject create(String name, String url,
-        JSObject params) /*-{
-        return new $wnd.OpenLayers.Layer.Grid(name, url, params);
-    }-*/;
+            JSObject params) /*-{
+     return new $wnd.OpenLayers.Layer.Grid(name, url, params);
+     }-*/;
 
     public static native void setTitleSize(JSObject self, int tileSize) /*-{
-        self.tileSize = tileSize;
-    }-*/;
+     self.tileSize = tileSize;
+     }-*/;
 
     public static native void setSingleTile(JSObject self, boolean singleTile) /*-{
-        self.singleTile = singleTile;
-    }-*/;
-    
+     self.singleTile = singleTile;
+     }-*/;
+
     public static native boolean isSingleTile(JSObject self) /*-{
-        return self.singleTile;
-    }-*/;
+     return self.singleTile;
+     }-*/;
 
     public static native void setRatio(JSObject self, float ratio) /*-{
-        self.ratio = ratio;
-    }-*/;
+     self.ratio = ratio;
+     }-*/;
 
     public static native void setBuffer(JSObject self, Integer buffer) /*-{
-        self.buffer = buffer;
-    }-*/;
+     self.buffer = buffer;
+     }-*/;
 
-    public static native void setNumLoadingTiles(JSObject self, Integer numLoadingTiles) /*-{
-        self.numLoadingTiles = numLoadingTiles;
-    }-*/;
+    public static native void setNumLoadingTiles(JSObject self,
+            Integer numLoadingTiles) /*-{
+     self.numLoadingTiles = numLoadingTiles;
+     }-*/;
 }

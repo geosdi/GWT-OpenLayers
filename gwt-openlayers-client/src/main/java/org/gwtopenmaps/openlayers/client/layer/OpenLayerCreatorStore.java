c2a26f5ba48ace04e760f9efa595104895e7b0b8
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-03-06 15:51:19 +0100

    Upgrade License

--- gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/OpenLayerCreatorStore.java
+++ gwt-openlayers-client/src/main/java/org/gwtopenmaps/openlayers/client/layer/OpenLayerCreatorStore.java
@@ -1,17 +1,18 @@
-/*
- * Copyright 2014 geoSDI.
+/**
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Copyright 2015 sourceforge.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package org.gwtopenmaps.openlayers.client.layer;
 
